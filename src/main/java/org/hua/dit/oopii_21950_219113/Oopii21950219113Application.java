c4afad26cc02e584a0aee3234ec508913ae0280c
liapisn <nikos@liapis.info>
2021-03-18 13:16:35 +0200

    first commit

---
+++ src/main/java/org/hua/dit/oopii_21950_219113/Oopii21950219113Application.java
@@ -0,0 +1,13 @@
+package org.hua.dit.oopii_21950_219113;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oopii21950219113Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Oopii21950219113Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/hua/dit/oopii_21950_219113/Oopii21950219113ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hua.dit.oopii_21950_219113;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oopii21950219113ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
