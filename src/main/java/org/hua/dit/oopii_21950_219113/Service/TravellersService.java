b53f58eda569fa63c99ea5d8f9333551e5c8f686
liapisn <nikos@liapis.info>
2021-04-23 20:54:51 +0300

    testing updates

--- src/main/java/org/hua/dit/oopii_21950_219113/Service/TravellersService.java
+++ src/main/java/org/hua/dit/oopii_21950_219113/Service/TravellersService.java
@@ -12,6 +12,7 @@ import javax.swing.text.TextAction;
 import java.io.IOException;
 import java.util.*;
 import java.util.function.Function;
+import java.util.jar.JarEntry;
 import java.util.stream.Collectors;
 
 import static java.util.Comparator.comparing;
@@ -24,7 +25,7 @@ import static java.util.stream.Collectors.toCollection;
  */
 @Component
 public class TravellersService {
-    private ArrayList<Traveller> travellers;
+    private ArrayList<Traveller> travellers=new ArrayList<>();
     @Autowired // Dependency injection
     private final CityRepository cityRepository;
 
@@ -42,8 +43,8 @@ public class TravellersService {
 
         CityService cityService = new CityService(cityRepository);
         HashMap<String, City> CitiesHashMap = (HashMap<String, City>) cityService.getCities().stream().collect(Collectors.toMap(City::getCityName, Function.identity()));
-
         JsonSaver jsc = new JsonSaver();
+        travellers=jsc.readJSON();
         /**
          * test travellers
          */
@@ -62,6 +63,8 @@ public class TravellersService {
 //        YoungTraveller Nick2 = new YoungTraveller(19,"Nick","Athens","gr",2,10,10,6,10,9,2,10,8,1);
 //        Nick2.compareCities(CitiesHashMap);
 //
+//        YoungTraveller Nick3 = new YoungTraveller(19,"Nick","Athens","gr",3,10,10,6,10,9,2,10,8,1);
+//        Nick3.compareCities(CitiesHashMap);
 //
 //        /* STANDARD ENTRIES */
 //        travellers.add(George);
@@ -73,21 +76,23 @@ public class TravellersService {
 //        travellers.add(Paul);
 //
 //        travellers.add(Nick2);
+//
+//        travellers.add(Nick3);
 //        /* END */
 
-
-
-        //jsc.writeJSON(travellers); // write to json
+//
+//
+//        jsc.writeJSON(travellers); // write to json
 
 //        travellers.clear(); //clear arraylist
 //
-        travellers = jsc.readJSON(); //fill it out reading from json file
-
+//        travellers = jsc.readJSON(); //fill it out reading from json file
+//
 //        for(Traveller traveller : travellers){
 //            System.out.println(traveller.getName() + " " + traveller.getTimeStamp());
 //        }
 //        System.out.println("After Sorting ");
-//
+//        travellers=removeDuplicateTravellers(travellers);
 //        for(Traveller traveller : travellers){
 //            System.out.println(traveller.getName() + " " + traveller.getTimeStamp());
 //        }
@@ -100,62 +105,51 @@ public class TravellersService {
         String FreeCity = "Amsterdam";
         String FreeCountry = "nl";
 
-        String SearchCity= "Cairo";
-        String SearchCountry = "eg";
-
-
-//        travellers.clear();
-//        travellers = jsc.readJSON();
-
-        //Shorting by time stamp
-        //Fast & compact way
-
-        //Exercise way.
+        String SearchCity= "Dubai";
+        String SearchCountry = "ae";
 
 
 
 
+        if(checkCityAvailability(SearchCity,SearchCountry)) //traveller searches for a city and in case that this city isn't into database the system adds it into database
+        {
+            System.out.println("This city ("+SearchCity+") is already into database ");
+        }
+        else
+        {
+            System.out.println("This city wasn't into database, now it is btw :)");
+            //so we update the hashMap to be up to date :)
+            CitiesHashMap = (HashMap<String, City>) cityService.getCities().stream().collect(Collectors.toMap(City::getCityName, Function.identity()));
+        }
 
-//
-//
-//        if(checkCityAvailability(SearchCity,SearchCountry)) //traveller searches for a city and in case that this city isn't into database the system adds it into database
-//        {
-//            System.out.println("This city ("+SearchCity+") is already into database ");
-//        }
-//        else
-//        {
-//            System.out.println("This city wasn't into database, now it is btw :)");
-//            //so we update the hashMap to be up to date :)
-//            CitiesHashMap = (HashMap<String, City>) cityService.getCities().stream().collect(Collectors.toMap(City::getCityName, Function.identity()));
-//        }
-//
-//        for (Traveller traveller : travellers)
-//        {
-//            bestCities= traveller.compareCities(CitiesHashMap);
-//            System.out.println("The best city for "+traveller.getName()+" is :"+bestCities.get(0).getCityName());
-//            bestCities=traveller.compareCities(3,bestCities);
-//            System.out.println("And the next 3 best cities for "+traveller.getName()+" are: ");
-//            for (City bestCity : bestCities)
-//            {
-//                System.out.println(bestCity.getCityName());
-//            }
-//        }
-//
-//        try {
-//            return ("And after all we have a free ticket for: "+FreeCity+" and the traveller that he gets it is: "+testTraveller.calculate_free_ticket(cityService.getCityByName(FreeCity.toUpperCase(),FreeCountry),travellers).getName());
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
+        for (Traveller traveller : travellers)
+        {
+            bestCities= traveller.compareCities(CitiesHashMap);
+            System.out.println("The best city for "+traveller.getName()+" is :"+bestCities.get(0).getCityName());
+            bestCities=traveller.compareCities(3,bestCities);
+            System.out.println("And the next 3 best cities for "+traveller.getName()+" are: ");
+            for (City bestCity : bestCities)
+            {
+                System.out.println(bestCity.getCityName());
+            }
+        }
 
-        //return travellers;
         travellers=removeDuplicateTravellers(travellers);
         jsc.writeJSON(travellers);
+
+        try {
+            return ("And after all we have a free ticket for: "+FreeCity+" and the traveller that he gets it is: "+testTraveller.calculate_free_ticket(cityService.getCityByName(FreeCity.toUpperCase(),FreeCountry),travellers).getName());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         return null;
     }
 
     public ArrayList<Traveller> getAllTravellers()
     {
-        return travellers;
+        JsonSaver jsc = new JsonSaver();
+        return jsc.readJSON();
     }
 
     public boolean checkCityAvailability (String cityName, String country)
